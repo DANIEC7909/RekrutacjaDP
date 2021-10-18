07a30b6926571711de9c6ee322c03f004295dde6
DANIEC7909 <46851343+DANIEC7909@users.noreply.github.com>
2021-10-18 22:27:52 +0200

    Update Agent.cs

--- DreamParable/Assets/Agent.cs
+++ DreamParable/Assets/Agent.cs
@@ -5,15 +5,36 @@ using UnityEngine;
 public class Agent : MonoBehaviour
 {
     Rigidbody rb;
+    [SerializeField] float speed=1, multiplayer=5;
+    bool xy;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        switch(Random.Range(0, 1))
+        {
+            case 0:
+                xy = true;
+                break;
+            case 1:
+                xy = false;
+                break;
+        }
     }
 
     
     void FixedUpdate()
     {
+        Vector3 pos = Vector3.zero;
+        if (xy)
+        {
+       pos = new Vector3(Mathf.Sin(Time.time )* multiplayer, 0,0);
+        }
+        else
+        {
+            pos = new Vector3(0, 0, Mathf.Sin(Time.time) * multiplayer);
+        }
 
-        rb.MovePosition(new Vector3(Mathf.PingPong(Time.deltaTime, 5), 0, 0));
+       
+        transform.position = Vector3.Lerp(transform.position, pos,Time.deltaTime*speed);
     }
 }
